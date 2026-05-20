a3e8b3de63cddf67644718e9f0a510398b3e7f5b
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-05-20 11:58:42 +0200

    Subida Inicial Proyecto

---
+++ back/src/main/java/com/metrica/porramundial/porramundialmetrica/PorraMundialMetricaApplication.java
@@ -0,0 +1,13 @@
+package com.metrica.porramundial.porramundialmetrica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorraMundialMetricaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PorraMundialMetricaApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/com/metrica/porramundial/porramundialmetrica/PorraMundialMetricaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metrica.porramundial.porramundialmetrica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorraMundialMetricaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
