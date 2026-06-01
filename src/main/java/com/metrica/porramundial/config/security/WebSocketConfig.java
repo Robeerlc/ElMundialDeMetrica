37e8a69665cfb518d116dca60d357430048f5fc4
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-06-01 11:51:36 +0200

    Change route for websocket

--- src/main/java/com/metrica/porramundial/config/security/WebSocketConfig.java
+++ src/main/java/com/metrica/porramundial/config/security/WebSocketConfig.java
@@ -30,7 +30,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
-                .setAllowedOrigins("https://porramundialmetrica-championsfinal-kb4lt-82bcf1-193-70-44-51.sslip.io")
+                .setAllowedOrigins("https://metrica-mundial-0dc78d-193-70-44-51.sslip.io")
                 .withSockJS();
     }
 
