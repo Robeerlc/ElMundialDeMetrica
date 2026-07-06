f92d5ed9528f615cda4132520623e2ddfb72eca5
Roberto Lucas <lucascastilloroberto@gmail.com>
2026-07-06 23:56:20 +0200

    Add correctSign check for prediction results

--- src/main/java/com/metrica/porramundial/service/ScoringService.java
+++ src/main/java/com/metrica/porramundial/service/ScoringService.java
@@ -53,16 +53,23 @@ public class ScoringService {
         int realHome = match.getHomeGoals();
         int realAway = match.getAwayGoals();
 
+        boolean correctSign = false;
+        if (predHome > predAway && realHome > realAway) correctSign = true;
+        else if (predHome < predAway && realHome < realAway) correctSign = true;
+        else if (predHome == predAway && realHome == realAway) correctSign = true;
+
         boolean correctWinner = Objects.equals(prediction.getWinningTeam(), match.getWinningTeam());
         boolean correctDiff = (predHome - predAway) == (realHome - realAway);
         boolean exactScore = predHome == realHome && predAway == realAway;
 
         PredictionResultType resultType;
-        if (!correctWinner) resultType = PredictionResultType.LOST;
+
+        if (!correctSign) resultType = PredictionResultType.LOST;
+        else if (!correctWinner) resultType = PredictionResultType.LOST;
         else if (exactScore) resultType = PredictionResultType.EXACT_MATCH;
         else if (correctDiff) resultType = PredictionResultType.GOAL_DIFFERENCE;
         else resultType = PredictionResultType.WINNER;
-
+        
         prediction.setPointsEarned(resultType.getPoints() * match.getPhase().getMultiplier());
         prediction.setResultType(resultType);
     }
